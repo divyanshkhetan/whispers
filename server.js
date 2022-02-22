8164d9e5dba79616ac5fe772657dc0697ffbc357
divyanshkhetan <divyanshkhetan18@gmail.com>
2022-02-22 20:55:14 +0530

    before deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 
 const app = express();
@@ -17,6 +18,17 @@ app.use(cookieParser());
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/messages', require('./routes/messages'));
+
+// Serve static assets in production 
+if (process.env.NODE_ENV === 'production') {
+
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + 'client' + 'build' + 'index.html')));
+
+}
+
 
 const PORT = process.env.PORT || 5000;
 
